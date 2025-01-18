077ab770a24e7b9f5fb4f0ad2e8c25f3be4bcc90
Kartik Kumar <kartikjhaba@gmail.com>
2025-01-18 22:13:25 +0530

    Add files via upload

---
+++ 009_personalProfile.js
@@ -0,0 +1,26 @@
+//Concept - Object Literal
+//Program - Create an object representing your personal profile. Include properties like name, age, city, hobbies, and favoriteColor. Then:
+// Add a new property isStudent with a value of true or false.
+// Update the value of city to a new city.
+// Delete the hobbies property.
+// Print the entire object to see the changes.
+
+const user = {
+    name : 'Kartik Kumar',
+    age : '50',
+    city : 'Mumbai',
+    hobbies : ['Guitar', 'Exercise', 'Javascript'],
+    favouriteColor : 'Blue'
+}
+
+// Add a new property isStudent with a value of true or false.
+user.isStudent = true;
+
+// Update the value of city to a new city.
+user.city = 'Delhi';
+
+// Delete the hobbies property.
+delete user.hobbies;
+
+// Print the entire object to see the changes.
+console.log(user);
